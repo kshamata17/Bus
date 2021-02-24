8f973d88abb03d7437847a2b350331fecb7e1c36
Project <kshamata@p.com>
2021-02-24 16:37:16 +0545

    location_track

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+/*Route::get('/', 'ProductController@list')->name('home');
+Route::post('checkout', 'OrderController@checkout')->name('checkout');*/
+
+/*Route::any('esewa/success', 'EsewaController@success')->name('esewa.success');
+Route::any('esewa/fail', 'EsewaController@fail')->name('esewa.fail');
+Route::get('payment/response', 'EsewaController@payment_response')->name('payment.response');*/
+
+/*Route::any('fonepay/return', 'FonepayController@fonepay_response')->name('fonepay.return');*/
+
